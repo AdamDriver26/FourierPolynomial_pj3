3bac0f93a7320a462b44c49a940d16852c4a9728
Adam Driver <adamjdriver26@gmail.com>
2019-04-03 19:56:30 +0100

    Completed up to question 1c, starting 1d.

--- FourierPolynomial.java
+++ FourierPolynomial.java
@@ -1,40 +1,118 @@
 import org.apache.commons.math3.*;
 
-public class FourierPolynomial extends RealFunction {
+public class FourierPolynomial extends RealFunction 
+{
+    /**
+     * The zeroth Fourier coefficient.
+     */
     double a0;
+    
+    /**
+     * An array of the cosine Fourier coefficients.
+     */
     double[] aj;
+    
+    /**
+     * An array of the sine Fourier coefficients.
+     */
     double[] bj;
     
-    FourierPolynomial(double zeroCoefficient, double[] aSeries, double[] bSeries){
+    /**
+     * Constructor for the class. 
+     * 
+     * @param zeroCoefficient value of the constant a0.
+     * @param aArray value of the aj array.
+     * @param bArray value of the bj array.
+     */
+    FourierPolynomial(double zeroCoefficient, double[] aArray, double[] bArray)
+    {
         a0 = zeroCoefficient;
-        aj = aSeries;
-        bj = bSeries;
+        // Ensures aj and bj are the same length using the lengthEqualiser method.
+        aj = lengthEqualiser(aArray, bArray.length);
+        bj = lengthEqualiser(bArray, aArray.length);
     }
     
-    public double getCoefficient(int j, boolean isOdd) { 
-    /*    if (j != 1){
-        int n = FourierPolynomial.aj.length;
-    }
-        
-        if (!isOdd && j == 0){
-            
-            
-            return a0;
+    /**
+     * Extends an array with zeros if it is shorter than the given value.
+     * 
+     * @param the array to be extended.
+     * @param the length the array should be extended to.
+     * @return an array extended with zeros to length n. 
+     */
+    private static double[] lengthEqualiser(double[] shortArray, int n)
+    {
+        // Simply returns the original array if it is not shorter than n.
+        if (shortArray.length >= n)
+        {
+            return shortArray;
         }
-        else if (!isOdd && j > 0){
-            return aj[j];
+        // Otherwise creates a new array of length n which includes the values from the original.
+        else
+        {
+            double[] extendedArray = new double[n];
+            for (int i=0; i<shortArray.length; i++)
+            {
+                extendedArray[i] = shortArray[i];
+            }
+            return extendedArray;
         }
-        else if (j > 0){
-            return bj[j];
-        } */
-        return 0.0;
     }
     
-    public double valueAt (double x, int n){
-        return 0.0;
+    /**
+     * 
+     */
+    public double getCoefficient(int j, boolean wantB)
+    {
+        int n = aj.length; //Could use either aj or bj since they are the same length when constructed.
+        if (j >= n)
+        {
+            return 0.0;
+        }
+        else 
+        {
+            if (!wantB && j==0)
+            {
+                return a0;
+            }
+            else if (!wantB)
+            {
+                return aj[j+1];
+            }
+            else
+            {
+                return bj[j+1];
+            }
+        }
     }
     
-
+    public double valueAt (double x)
+    {
+        int n = aj.length;
+        double value = a0/2.0;
+        for (int j=1; j<n; j++)
+        {
+            value += aj[j]*Math.cos(j*x) + bj[j]*Math.sin(j*x);
+        }
+        return value;
+    }
+    
+    public double derivativeValueAt (double x)
+    {
+        int n = aj.length;
+        double value = 0.0;
+        for (int j=1; j<n; j++)
+        {
+            value += bj[j]*Math.cos(j*x) - aj[j]*Math.sin(j*x);
+        }
+        return value;
+    }
+    
+    public FourierPolynomial add (FourierPolynomial f)
+    {   
+        f.a0 += this.a0;
+        // ......
+        return f;
+    }
 }
 
 
